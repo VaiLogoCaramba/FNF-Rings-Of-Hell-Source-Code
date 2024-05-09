a5a4ca6122a1260e3700d73d9ce829546cbdea83
VaiLogoCaramba <150534279+VaiLogoCaramba@users.noreply.github.com>
2024-05-09 19:28:14 -0300

    Update Unlockables.hx

--- source/backend/Unlockables.hx
+++ source/backend/Unlockables.hx
@@ -1,3 +1,4 @@
 package backend;
 
 //WIP unlocables for Menus, Etc | P.S. Maybe it's better to remove Sound Test and add something better? like galery lol
+//Ilocalizables WIP para menús, etc. | P.D. ¿Quizás sea mejor eliminar Sound Test y agregar algo mejor? como galería jajaja
